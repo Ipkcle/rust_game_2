b3affe567d13dd5f939d30408ecec877b883a2a5
Adrian Fullmer <ipkcle@gmail.com>
2018-06-27 11:23:44 -0600

    removed unused action.rs

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -1,4 +1,4 @@
-use components::{combat::*, physics::*};
+use components::{deletion_conditions::MarkedForDeletion, combat::*, physics::*};
 use resources::DeltaTime;
 use specs::Entities;
 use specs::LazyUpdate;
@@ -17,6 +17,26 @@ impl<'a> System<'a> for ShootBullets {
         Read<'a, LazyUpdate>,
         ReadStorage<'a, Position>,
         WriteStorage<'a, CanShoot>,
+    );
+
+    fn run(&mut self, (dt, entities, updater, position, mut can_shoot): Self::SystemData) {
+        use specs::Join;
+
+        for (entity, pos, can_shoot) in (&*entities, &position, &mut can_shoot).join() {
+            can_shoot.update(pos, dt.get(), &entities, &updater);
+        }
+    }
+}
+
+pub struct HandleDeath;
+
+impl<'a> System<'a> for HandleDeath {
+    type SystemData = (
+        ReadExpect<'a, DeltaTime>,
+        Entities<'a>,
+        Read<'a, LazyUpdate>,
+        ReadStorage<'a, Health>,
+        WriteStorage<'a, MarkedForDeletion>,
     );
 
     fn run(&mut self, (dt, entities, updater, position, mut can_shoot): Self::SystemData) {
